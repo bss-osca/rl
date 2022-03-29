6893bb5742ac9ce73bfafb2b068b107762d8e716
fisherman1810 <junk1@relund.dk>
2022-03-29 11:24:14 +0200

    Copy slides

--- book/render_book.R
+++ book/render_book.R
@@ -1,6 +1,15 @@
 sink("render_book.log", append=F, split=T)
 sessionInfo()
-setwd("book")
-bookdown::render_book("index.Rmd", quiet = TRUE)
+
+## render book
+bookdown::render_book("book", quiet = TRUE)
+
+## copy slides to _book
+dir.create("book/_book/slides", showWarnings = FALSE)
+setwd("slides")
+file.copy(list.files(pattern = ".html"), "../book/_book/slides/", overwrite = T)
+file.copy("libs", "../book/_book/slides/", recursive = T)
+file.copy("img", "../book/_book/slides/", recursive = T)
+
 warnings()
 sink()
