08ce80cb017b2f74f68f02a016afbef9c4abb76e
relund <junk@relund.dk>
2022-04-05 10:24:27 +0200

    Update

--- setup-main.R
+++ setup-main.R
@@ -1,4 +1,4 @@
+#### How to install the package ####
 devtools::install("package/rl/", quick = T, force = T)   # local
 remotes::install_github("bss-osca/rl/package/rl", upgrade = FALSE)      # github
 renv::snapshot(prompt = FALSE, force = TRUE)
-
