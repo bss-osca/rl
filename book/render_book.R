764dc4434000e1e2207b430e35d1ce7692045232
Lars Relund <junk@relund.dk>
2022-10-17 15:50:49 +0200

    Add script for misc files

--- book/render_book.R
+++ book/render_book.R
@@ -6,12 +6,20 @@ sessionInfo()
 ## render book
 bookdown::render_book("book", quiet = TRUE)
 
-## copy slides to _book
+## copy slides 
 dir.create("book/_book/slides", showWarnings = FALSE)
 setwd("slides")
 file.copy(list.files(pattern = ".html"), "../book/_book/slides/", overwrite = T)
 file.copy("libs", "../book/_book/slides/", recursive = T)
 file.copy("img", "../book/_book/slides/", recursive = T)
+
+### copy misc stuff 
+setwd("..")
+dir.create("book/_book/misc", showWarnings = FALSE)
+setwd("exam")
+file.copy(list.files(pattern = ".html"), "../book/_book/misc/", overwrite = T)
+setwd("../project")
+file.copy(list.files(pattern = ".html"), "../book/_book/misc/", overwrite = T)
 
 warnings()
 sink()
