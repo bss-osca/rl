deeade11edcad60dc9bbcf13970bdf8cb1320aad
Lars Relund <junk@relund.dk>
2022-12-02 15:31:46 +0100

    Update packages

--- setup-main.R
+++ setup-main.R
@@ -1,4 +1,4 @@
 
 remotes::install_github("gadenbuie/countdown")
-remotes::install_github("mitchelloharawild/icons")
+remotes::install_github("mitchelloharawild/icons", force = T)
 remotes::install_github("ROpenSci/bibtex")
